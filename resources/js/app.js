1f0eb791c417b11827c64454364287c584f34bba
jurdunnn <jordy.downs@hotmail.co.uk>
2023-08-27 20:15:11 +0100

    Initial Commit

---
+++ resources/js/app.js
@@ -0,0 +1,124 @@
+import './bootstrap';
+
+const sections = document.querySelectorAll("section");
+const images = document.querySelectorAll(".bg");
+const headings = gsap.utils.toArray(".section-heading");
+const outerWrappers = gsap.utils.toArray(".outer");
+const innerWrappers = gsap.utils.toArray(".inner");
+const clamp = gsap.utils.clamp(0, sections.length - 1);
+
+document.addEventListener("wheel", handleWheel);
+document.addEventListener("touchstart", handleTouchStart);
+document.addEventListener("touchmove", handleTouchMove);
+document.addEventListener("touchend", handleTouchEnd);
+
+let animating = false,
+    currentIndex = -1;
+
+const touch = {
+    startX: 0,
+    startY: 0,
+    dx: 0,
+    dy: 0,
+    startTime: 0,
+    dt: 0
+};
+
+const tlDefaults = {
+    ease: "slow.inOut",
+    duration: 1.25
+};
+
+const splitHeadings = headings.map((heading) => {
+    return new SplitText(heading, {
+        type: "chars, words, lines",
+        linesClass: "clip-text"
+    });
+});
+
+console.log(splitHeadings);
+
+gsap.set(outerWrappers, {yPercent: 100});
+gsap.set(innerWrappers, {yPercent: -100});
+
+function gotoSection(index, direction) {
+    index = clamp(index); // make sure it's valid
+
+    // we're at the end, so exit
+    if (currentIndex === index) {
+        return;
+    }
+
+    animating = true;
+
+    let fromTop = direction === -1,
+        dFactor = fromTop ? -1 : 1,
+        tl = gsap.timeline({
+            defaults: {duration: 1.25, ease: "power1.inOut"},
+            onComplete: () => (animating = false)
+        });
+
+    if (currentIndex >= 0) {
+        // The first time this function runs, current is -1
+        gsap.set(sections[currentIndex], {zIndex: 0});
+        tl.to(images[currentIndex], {yPercent: -15 * dFactor}).set(
+            sections[currentIndex],
+            {autoAlpha: 0}
+        );
+    }
+
+    gsap.set(sections[index], {autoAlpha: 1, zIndex: 1});
+    tl.fromTo(
+        [outerWrappers[index], innerWrappers[index]],
+        {yPercent: (i) => (i ? -100 * dFactor : 100 * dFactor)},
+        {yPercent: 0},
+        0
+    )
+        .fromTo(images[index], {yPercent: 15 * dFactor}, {yPercent: 0}, 0)
+        .fromTo(
+            splitHeadings[index].chars,
+            {autoAlpha: 0, yPercent: 150 * dFactor},
+            {
+                autoAlpha: 1,
+                yPercent: 0,
+                duration: 1,
+                ease: "power2",
+                stagger: {
+                    each: 0.02,
+                    from: "random"
+                }
+            },
+            0.2
+        );
+
+    currentIndex = index;
+}
+
+function handleWheel(e) {
+    if (animating) return;
+
+    e.wheelDeltaY < 0
+        ? gotoSection(currentIndex + 1, 1)
+        : gotoSection(currentIndex - 1, -1);
+}
+
+function handleTouchStart(e) {
+    const t = e.changedTouches[0];
+    touch.startX = t.pageX;
+    touch.startY = t.pageY;
+}
+
+function handleTouchMove(e) {
+    e.preventDefault();
+}
+
+function handleTouchEnd(e) {
+    if (animating) return;
+    const t = e.changedTouches[0];
+    touch.dx = t.pageX - touch.startX;
+    touch.dy = t.pageY - touch.startY;
+    if (touch.dy > 10) gotoSection(currentIndex - 1, -1);
+    if (touch.dy < -10) gotoSection(currentIndex + 1, 1);
+}
+
+gotoSection(0, 1);
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Jordan Downs</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+
+    <body class="antialiased scroll-smooth">
+        <header>
+            <div>Animated Sections</div>
+            <div>Made By Brian</div>
+        </header>
+        <section class="first">
+            <div class="outer">
+                <div class="inner">
+                    <div class="bg one">
+                        <h2 class="section-heading">Scroll down</h2>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+        <section class="second">
+            <div class="outer">
+                <div class="inner">
+                    <div class="bg">
+                        <h2 class="section-heading">Animated with GSAP</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="third">
+            <div class="outer">
+                <div class="inner">
+                    <div class="bg">
+                        <h2 class="section-heading">GreenSock</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="fourth">
+            <div class="outer">
+                <div class="inner">
+                    <div class="bg">
+                        <h2 class="section-heading">Animation platform</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="fifth">
+            <div class="outer">
+                <div class="inner">
+                    <div class="bg">
+                        <h2 class="section-heading">Keep scrolling</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
