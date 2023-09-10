f3e2e4a6de8a74d4568b509424ba181d13f02378
jurdunnn <jordy.downs@hotmail.co.uk>
2023-09-10 21:37:40 +0100

    Remove heading class from non-heading sections

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -10,14 +10,14 @@
             <div class="bg bg-{{ $bgColor }} one flex flex-col">
                 <div class="slides-container">
                     <h2 class="slide section-heading text-{{ $textColor }}">{{ $heading }}</h2>
-                    <h2 class="slide section-heading text-{{ $textColor }}">Slide</h2>
-                    <h2 class="slide section-heading text-{{ $textColor }}">Slide</h2>
-                    <h2 class="slide section-heading text-{{ $textColor }}">Slide</h2>
-                    <h2 class="slide section-heading text-{{ $textColor }}">Slide</h2>
+                    <h2 class="slide text-{{ $textColor }}">Slide</h2>
+                    <h2 class="slide text-{{ $textColor }}">Slide</h2>
+                    <h2 class="slide text-{{ $textColor }}">Slide</h2>
+                    <h2 class="slide text-{{ $textColor }}">Slide</h2>
                 </div>
                 <div class="controls">
-                    <button id="prevButton">Prev</button>
-                    <button id="nextButton">Next</button>
+                    <button class="prevButton">Prev</button>
+                    <button class="nextButton">Next</button>
                 </div>
             </div>
         </div>
