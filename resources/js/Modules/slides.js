0011f9925d3fb217a9693626b6e68fdc6e97f41f
jurdunnn <jordy.downs@hotmail.co.uk>
2023-09-11 18:07:39 +0100

    Rename count to slideIndex

--- resources/js/Modules/slides.js
+++ resources/js/Modules/slides.js
@@ -4,14 +4,14 @@ let index = currentIndex.getCurrentIndex();
 
 const sections = document.querySelectorAll('section');
 let section = sections[index];
-let count = 0;
+let slideIndex = 0;
 let targets = section.querySelectorAll(".slide");
 
 section.querySelector('.nextButton').addEventListener("click", slideOneNext);
 section.querySelector('.prevButton').addEventListener("click", slideOnePrev);
 
 function handleCurrentIndexChange(updatedIndex) {
-    count = 0;
+    slideIndex = 0;
 
     index = updatedIndex;
 
@@ -34,19 +34,19 @@ gsap.set(targets, {xPercent: 100});
 gsap.set(targets[0], {xPercent: 0});
 
 function slideOneNext() {
-    gsap.fromTo(targets[count], {xPercent: 0, zIndex: 0}, {delay: 0.2, duration: 0.8, xPercent: -100, zIndex: -10});
+    gsap.fromTo(targets[slideIndex], {xPercent: 0, zIndex: 0}, {delay: 0.2, duration: 0.8, xPercent: -100, zIndex: -10});
 
-    count = count < targets.length - 1 ? ++count : 0;
+    slideIndex = slideIndex < targets.length - 1 ? ++slideIndex : 0;
 
-    gsap.fromTo(targets[count], {xPercent: 100, zIndex: 10}, {duration: 0.8, xPercent: 0, zIndex: 0});
+    gsap.fromTo(targets[slideIndex], {xPercent: 100, zIndex: 10}, {duration: 0.8, xPercent: 0, zIndex: 0});
 }
 
 function slideOnePrev() {
-    if (count === 0) return;
+    if (slideIndex === 0) return;
 
-    gsap.fromTo(targets[count], {xPercent: 0, zIndex: 10}, {xPercent: 100, zIndex: 0});
+    gsap.fromTo(targets[slideIndex], {xPercent: 0, zIndex: 10}, {xPercent: 100, zIndex: 0});
 
-    count = count < targets.length ? --count : 0;
+    slideIndex = slideIndex < targets.length ? --slideIndex : 0;
 
-    gsap.fromTo(targets[count], {xPercent: -100, zIndex: 0}, {delay: 0, duration: 0.8, xPercent: 0, zIndex: -10});
+    gsap.fromTo(targets[slideIndex], {xPercent: -100, zIndex: 0}, {delay: 0, duration: 0.8, xPercent: 0, zIndex: -10});
 }
