99d2bc8286bb4004814522d4931affeb3fbdda75
jurdunnn <jordy.downs@hotmail.co.uk>
2023-09-17 13:38:15 +0100

    Build text block

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,6 +1,6 @@
 @props([
-    'bgColor' => 'bg-primary',
-    'textColor' => 'text-secondary',
+    'bgColor' => 'primary',
+    'textColor' => 'secondary',
     'heading' => 'Section',
     'slides' => null,
 ])
@@ -11,28 +11,41 @@
             <div class="bg bg-{{ $bgColor }} one flex flex-col">
                 <div class="slides-container">
                     <div class="slide">
-                        <h2 class="section-heading max-w-3xl mx-auto text-{{ $textColor }}">{{ $heading }}</h2>
+                        <h2 class="section-heading text-center max-w-3xl xl:max-w-4xl mx-auto text-{{ $textColor }}">{{ $heading }}</h2>
                     </div>
 
                     @foreach($slides as $slide)
                         <div id="slide-{{ $loop->index }}" class="slide text-{{ $textColor }}">
                             @switch($slide->component[0]->value)
                                 @case('text')
-                                    <div class="text-component">
-                                        <p>{{ $slide->component_data['text'] }}</p>
+                                    <div class="flex flex-col justify-center w-2/4 px-8 pt-2 pb-6 mx-auto max-h-[60vh] overflow-hidden text-lg text-center border-0 shadow-xl rounded-xl bg-{{ $textColor }} text-{{ $bgColor }} gap-y-6">
+                                        @isset($slide->component_data['heading'])
+                                            <h3 class="text-xl font-semibold">{{ $slide->component_data['heading'] }}</h3>
+                                        @endisset
+
+                                        @isset($slide->component_data['text'])
+                                            <p class="text-left normal-case">{{ $slide->component_data['text'] }}</p>
+                                        @endisset
                                     </div>
                                 @break
 
                                 @case('text-with-image')
                                     <div class="text-with-image-component">
-                                        <p>{{ $slide->component_data['text'] }}</p>
-                                        <p>{{ $slide->component_data['image'] }}</p>
+                                        @isset($slide->component_data['text'])
+                                            <p>{{ $slide->component_data['text'] }}</p>
+                                        @endisset
+
+                                        @isset($slide->component_data['image'])
+                                            <p>{{ $slide->component_data['image'] }}</p>
+                                        @endisset
                                     </div>
                                 @break
 
                                 @default
                                     <div class="default-component">
-                                        <p>{{ $slide->component_data['text'] }}</p>
+                                        @isset($slide->component_data['text'])
+                                            <p>{{ $slide->component_data['text'] }}</p>
+                                        @endisset
                                     </div>
                             @endswitch
                         </div>
