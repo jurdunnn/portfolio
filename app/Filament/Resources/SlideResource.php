c99dfb133d274b3c93c0bc682d292658a8d1bfb1
jurdunnn <jordy.downs@hotmail.co.uk>
2023-09-17 15:04:57 +0100

    Remove unused includes

--- app/Filament/Resources/SlideResource.php
+++ app/Filament/Resources/SlideResource.php
@@ -4,15 +4,12 @@ namespace App\Filament\Resources;
 
 use App\Enums\ComponentType;
 use App\Filament\Resources\SlideResource\Pages;
-use App\Filament\Resources\SlideResource\RelationManagers;
 use App\Models\Slide;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class SlideResource extends Resource
 {
