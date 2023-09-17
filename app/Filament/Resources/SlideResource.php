3a8b4e786d9bc9ea4631c5b30d6f6a8258313100
jurdunnn <jordy.downs@hotmail.co.uk>
2023-09-17 15:04:37 +0100

    Remove reordable on component data

--- app/Filament/Resources/SlideResource.php
+++ app/Filament/Resources/SlideResource.php
@@ -32,8 +32,7 @@ class SlideResource extends Resource
                     ->default(0)
                     ->numeric(),
                 Forms\Components\Select::make('component')->options(ComponentType::class)->columnSpanFull(),
-                Forms\Components\KeyValue::make('component_data')
-                    ->reorderable(),
+                Forms\Components\KeyValue::make('component_data'),
                 Forms\Components\Textarea::make('component_data.text')
                     ->rows(15),
             ]);
