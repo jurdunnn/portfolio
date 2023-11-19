4464e9ef0489f58765b8258a133469be353f6e23
jurdunnn <jordy.downs@hotmail.co.uk>
2023-11-19 09:53:09 +0000

    Adjust cloud word spacing and size

--- resources/views/components/word-cloud.blade.php
+++ resources/views/components/word-cloud.blade.php
@@ -7,7 +7,7 @@
     <ul class="flex flex-wrap justify-center max-w-xl cloud align-center gap-2 leading-8">
         @foreach(explode(',', $slide->component_data['words']) as $word)
             <li>
-                <a href="https://google.co.uk/search?q=laravel {{ trim($word) }}" target="_blank" class="hover:scale-110 hover:font-bold ease-in-out duration-150" data-weight="{{ $loop->index }}">{{ trim($word) }}</a>
+                <a href="https://google.co.uk/search?q=laravel {{ trim($word) }}" target="_blank" class="mx-4 my-2 hover:scale-110 hover:font-bold ease-in-out duration-150" data-weight="{{ $loop->index }}">{{ trim($word) }}</a>
             </li>
         @endforeach
     </ul>
@@ -17,7 +17,7 @@
     const tags = document.querySelectorAll('ul.cloud a');
 
     tags.forEach((tag, index) => {
-        const size = Math.floor(Math.random() * 41) + 10;
+        const size = Math.floor(Math.random() * 80) + 20;
 
         const hue = Math.floor(Math.random() * 360);
 
