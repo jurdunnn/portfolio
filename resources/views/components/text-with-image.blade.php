57c7e627e6117bf7d02adb0e3ae9dce22444b0aa
jurdunnn <jordy.downs@hotmail.co.uk>
2023-11-20 22:16:47 +0000

    Adjust text and image styling to look better on mobile

--- resources/views/components/text-with-image.blade.php
+++ resources/views/components/text-with-image.blade.php
@@ -1,8 +1,8 @@
 @props(['slide', 'bgColor', 'textColor'])
 
-<div class="flex flex-col lg:flex-row justify-between w-3/4 max-h-[60vh] gap-8">
+<div class="flex flex-col lg:flex-row flex-col-reverse justify-between w-3/4 max-h-[80vh] lg:max-h-[60vh] gap-8">
     @isset($slide->component_data['text'])
-    <div class="justify-center w-full no-scroll lg:w-2/3 p-2 lg:p-8 mx-auto h-[30vh] lg:h-[60vh] overflow-scroll text-ellipsis text-normal lg:text-lg text-center border-0 shadow-xl rounded-xl bg-{{ $textColor }} text-{{ $bgColor }}">
+    <div class="justify-center w-full no-scroll lg:w-2/3 p-2 lg:p-8 mx-auto h-[90vh] md:h-[60vh] overflow-scroll text-ellipsis text-sm md:text-base lg:text-lg text-center border-0 shadow-xl rounded-xl bg-{{ $textColor }} text-{{ $bgColor }}">
         <div class="flex flex-col no-scroll gap-y-2 lg:gap-y-6">
             @isset($slide->component_data['heading'])
             <h3 class="font-semibold no-scroll text-normal lg:text-lg">{{ $slide->component_data['heading'] }}</h3>
@@ -16,7 +16,7 @@
 @endisset
 
 @isset($slide->component_data['image'])
-<div class="flex flex-col justify-center w-full mx-auto h-[90vh] lg:h-[60vh] overflow-hidden text-lg text-center border-0 shadow-xl rounded-xl bg-{{ $textColor }} text-{{ $bgColor }} gap-y-6">
+<div class="flex flex-col justify-center w-full mx-auto h-[60vh] overflow-hidden text-lg text-center border-0 shadow-xl rounded-xl bg-{{ $textColor }} text-{{ $bgColor }} gap-y-6">
     <img
         src="{{ Storage::url($slide->component_data['image']) }}"
         @isset($slide->component_data['image_alt'])
