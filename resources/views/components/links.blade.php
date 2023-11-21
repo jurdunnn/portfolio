8a6868b47256fca1850d358565b5c41f91b22914
jurdunnn <jordy.downs@hotmail.co.uk>
2023-11-21 19:39:36 +0000

    Add more breakpoints for the text and button widths

--- resources/views/components/links.blade.php
+++ resources/views/components/links.blade.php
@@ -1,14 +1,14 @@
 @props(['slide', 'bgColor', 'textColor'])
 
 <div class="flex flex-col items-center justify-center w-full gap-y-8">
-    <h1 class="w-2/3 md:w-1/2 text-2xl font-bold text-center md:text-4xl md:w-1/3">{{ $slide->project->heading }}</h1>
+    <h1 class="w-2/3 text-2xl font-bold text-center md:w-1/2 md:text-3xl lg:text-4xl xl:w-1/3">{{ $slide->project->heading }}</h1>
 
-    <p class="w-2/3 md:w-1/2 text-xl text-center normal-case md:w-1/4">
+    <p class="w-2/3 text-xl text-center normal-case md:w-1/2 xl:w-1/3">
         Thank you for taking an interest in {{ $slide->project->heading }}! Feel free to have a look at the code or checkout the webiste.
         If you have any questions, please feel free to reach out to me on <a href="https://www.linkedin.com/in/jordan-downs-5a546a104/" class="underline no-slide" target="_blank">LinkedIn</a>.
     </p>
 
-    <div class="flex no-slide mx-auto text-center mt-8 font-bold text-xs sm:text-sm lg:text-base  gap-x-12 w-full sm:w-2/3 md:w-1/2 px-6 md:px-12 text-{{ $textColor }}">
+    <div class="flex flex-col lg:flex-row gap-y-3 no-slide mx-auto text-center mt-8 font-bold text-xs sm:text-sm lg:text-base  gap-x-12 w-full w-2/3 md:w-1/2 px-6 lg:px-12 text-{{ $textColor }}">
         <a
             @isset($slide->component_data['github'])
                 href="{{ $slide->component_data['github'] }}"
