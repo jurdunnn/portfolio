983c3a43d7e849842d3de23ed0ef26f15fac57fd
jurdunnn <jordy.downs@hotmail.co.uk>
2023-11-21 19:18:36 +0000

    Fix indentation on text component

--- resources/views/components/text.blade.php
+++ resources/views/components/text.blade.php
@@ -3,11 +3,11 @@
 <div class="justify-center w-3/4 p-8 no-scroll mx-auto h-[60vh] overflow-scroll text-ellipsis text-normal xl:text-lg text-center border-0 shadow-xl rounded-xl bg-{{ $textColor }} text-{{ $bgColor }}">
     <div class="flex flex-col gap-y-6">
         @isset($slide->component_data['heading'])
-        <h3 class="font-semibold no-scroll text-normal">{{ $slide->component_data['heading'] }}</h3>
-    @endisset
+            <h3 class="font-semibold no-scroll text-normal">{{ $slide->component_data['heading'] }}</h3>
+        @endisset
 
-    @isset($slide->component_data['text'])
-    <p class="text-lg text-left normal-case md:text-xl no-scroll">{!! $slide->component_data['text'] !!}</p>
-@endisset
+        @isset($slide->component_data['text'])
+            <p class="text-lg text-left normal-case md:text-xl no-scroll">{!! $slide->component_data['text'] !!}</p>
+        @endisset
     </div>
 </div>
